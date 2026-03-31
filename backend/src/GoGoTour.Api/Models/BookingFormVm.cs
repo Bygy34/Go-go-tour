0e88e6b7d626fb6f594c84543499224c1af37f9a
Dmytro Zolotukhin <dmytro.zolotukhin@nixs.com>
2026-03-31 21:09:21 +0200

    fixed issue

--- backend/src/GoGoTour.Api/Models/BookingFormVm.cs
+++ backend/src/GoGoTour.Api/Models/BookingFormVm.cs
@@ -6,6 +6,7 @@ public class BookingFormVm
 {
     public int TourId { get; set; }
 
+    [RegularExpression(@"^\+381\d{9}$", ErrorMessage = "Phone number must start with +380 and contain 13 digits total.")]
     [Required]
     public string FullName { get; set; } = string.Empty;
 
